85a0ddd1c9df8f8035da91bcd436a834c94e720c
FelipeHoch <hochfelipe@gmail.com>
2022-01-28 18:56:12 -0300

    Created extract component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NovaTransferenciaComponent,
+    ExtratoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
+  valor: number = 10;
+  destino: number = 10;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limpaCampos();
+  }
+
+  limpaCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
