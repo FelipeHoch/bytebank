46324e8bac6f7ab5006e23cc62ddf4ddd2ac1a65
FelipeHoch <hochfelipe@gmail.com>
2022-01-30 12:18:09 -0300

    Table created to display transfers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencia: any;
+  transferencias: any[] = [];
 
   transferir($event: any) {
     console.log($event);
-    this.transferencia = $event
+    const transferencia = {...$event, data: new Date()}
+    this.transferencias.push(transferencia)
   }
 }
--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent implements OnInit {
+  @Input() transferencias: any[] = [];
 
   constructor() { }
 
